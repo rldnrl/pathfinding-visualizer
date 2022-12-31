769aa0a62ca1fdfe3e352baae7ce52408b806596
rldnrl <devpark1002@gmail.com>
2022-12-31 18:18:21 +0900

    Initial Commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import PathfindingVisualizer from "./PathfindingVisualizer/PathfindingVisualizer";
+import "./App.css";
+
+export default function App() {
+  return (
+    <div className="app">
+      <PathfindingVisualizer />
+    </div>
+  );
+}
